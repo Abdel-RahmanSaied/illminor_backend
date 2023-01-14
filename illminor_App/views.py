9d8f8200a0d97bb7f1f7f377cac9ad1053f97b53
Abdel-Rahman Saied <abdo.am169@gmail.com>
2023-01-14 16:00:30 +0200

    .

--- illminor_App/views.py
+++ illminor_App/views.py
@@ -193,24 +193,24 @@ class alzhimarTest_ViewSet(viewsets.ModelViewSet):
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
 
-
-class heartTest_ViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
-    queryset = heartTest.objects.all()
-    serializer_class = heartTestSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    pagination_class = StandardResultsSetPagination
-    filterset_fields = ['user__id','user__username', 'id', 'date', "result"]
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid() :
-            result = serializer.predict()
-            serializer.save(result=result)
-            return Response({"response": result}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return super().update(request, *args, **kwargs)
-
+#
+# class heartTest_ViewSet(viewsets.ModelViewSet):
+#     permission_classes = [IsAuthenticated]
+#     queryset = heartTest.objects.all()
+#     serializer_class = heartTestSerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+#     pagination_class = StandardResultsSetPagination
+#     filterset_fields = ['user__id','user__username', 'id', 'date', "result"]
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         if serializer.is_valid() :
+#             result = serializer.predict()
+#             serializer.save(result=result)
+#             return Response({"response": result}, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response({"response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def update(self, request, *args, **kwargs):
+#         kwargs['partial'] = True
+#         return super().update(request, *args, **kwargs)
+#
